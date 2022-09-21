c55af8797007f5acb8c68a6acff419f8d12622fc
Sensei205 <maxim55miui11@gmail.com>
2022-09-22 00:50:45 +0300

    PixelPropsUtils: Remove spoofing for currently supported Pixel devices

--- core/java/com/android/internal/util/arrow/PixelPropsUtils.java
+++ core/java/com/android/internal/util/arrow/PixelPropsUtils.java
@@ -31,6 +31,7 @@ import java.util.Map;
 public class PixelPropsUtils {
 
     private static final String TAG = PixelPropsUtils.class.getSimpleName();
+	private static final String DEVICE = "ro.product.device";
     private static final boolean DEBUG = false;
 
 	private static final Map<String, Object> propsToChange;
@@ -117,6 +118,18 @@ public class PixelPropsUtils {
             "com.mobile.legends",
             "com.tencent.tmgp.sgame"
     };
+	
+	// Codenames for currently supported Pixels by Google
+    private static final String[] pixelCodenames = {
+            "oriole",
+            "raven",
+            "redfin",
+            "barbet",
+            "bramble",
+            "sunfish",
+            "coral",
+            "flame"
+    };
 
     private static volatile boolean sIsGms = false;
 
@@ -169,14 +182,18 @@ public class PixelPropsUtils {
         }
         if (packageName.startsWith("com.google.")
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
+					
+			boolean isPixelDevice = Arrays.asList(pixelCodenames).contains(SystemProperties.get(DEVICE));
 
             if (packageName.equals("com.google.android.apps.photos")) {
                 if (SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
                     propsToChange.putAll(propsToChangePixelXL);
                 } else {
+					if (isPixelDevice) return;
                     propsToChange.putAll(propsToChangePixel5);
                 }
             } else {
+				if (isPixelDevice) return;
                 if (Arrays.asList(packagesToChangePixel6).contains(packageName)) {
                     propsToChange.putAll(propsToChangePixel6);
                 } else if (Arrays.asList(packagesToChangePixelXL).contains(packageName)) {
